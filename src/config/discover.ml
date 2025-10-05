1abc94f3c395e1659e65c6d3edcfd2ffcf6c25db
Naora <jorisgundermann@gmail.com>
2025-10-05 14:36:54 +0200

    remove a cflag add ocaml 5 as minimal dep

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,33 +1,7 @@
 module C = Configurator.V1
 
-(* flags:
-    -fPIC
-    -I/usr/include/glib-2.0
-    -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
-    -I/usr/include/libmount
-    -I/usr/include/blkid
-    -I/usr/include/webp
-    -I/usr/include/pango-1.0
-    -I/usr/include/harfbuzz
-    -I/usr/include/freetype2
-    -I/usr/include/libpng16
-    -I/usr/include/fribidi
-    -I/usr/include/cairo
-    -I/usr/include/pixman-1
-    -I/usr/include/x86_64-linux-gnu
-    -I/usr/include/librsvg-2.0
-    -I/usr/include/gdk-pixbuf-2.0
-    -I/usr/include/hdf5/serial
-    -I/usr/include/OpenEXR
-    -pthread
-    -I/usr/include/Imath
-    -I/usr/include/openjpeg-2.5
-    -DHWY_SHARED_DEFINE *)
-
-(* c_library_flags: -lvips -lgio-2.0 -lgobject-2.0 -lglib-2.0 *)
-
 let libs = [ "-lvips"; "-lgio-2.0"; "-lgobject-2.0"; "-lglib-2.0" ]
-let cflags = [ "-fPIC"; "-DHWY_SHARED_DEFINE" ]
+let cflags = [ "-fPIC" ]
 
 let () =
   C.main ~name:"vips" (fun c ->
@@ -40,6 +14,5 @@ let () =
             | None -> default
             | Some deps -> deps)
       in
-
       C.Flags.write_sexp "c_flags.sexp" conf.cflags;
       C.Flags.write_sexp "c_library_flags.sexp" conf.libs)

5e35446f15ac19439f080e666cf03c347c6a61c9
Naora <jorisgundermann@gmail.com>
2025-10-06 14:45:52 +0200

    fix build fedora

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,17 +1,13 @@
 module C = Configurator.V1
 
-let libs = [ "-lvips"; "-lgio-2.0"; "-lgobject-2.0"; "-lglib-2.0" ]
-let cflags = [ "-fPIC" ]
-
 let () =
   C.main ~name:"vips" (fun c ->
-      let default : C.Pkg_config.package_conf = { libs; cflags } in
       let conf =
         match C.Pkg_config.get c with
-        | None -> default
+        | None -> failwith "pkg-config not found"
         | Some pc -> (
             match C.Pkg_config.query pc ~package:"vips" with
-            | None -> default
+            | None -> failwith "vips not found"
             | Some deps -> deps)
       in
       C.Flags.write_sexp "c_flags.sexp" conf.cflags;
